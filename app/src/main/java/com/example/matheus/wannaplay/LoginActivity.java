46658bb7f9a4b1b877d28b1694dd23cd1c86e7f0
Matheus Ruppenthal <ruppenthalmatheus@gmail.com>
2018-09-30 03:00:31 -0300

    Preferences Fragment components config

--- app/src/main/java/com/example/matheus/wannaplay/LoginActivity.java
+++ app/src/main/java/com/example/matheus/wannaplay/LoginActivity.java
@@ -5,11 +5,14 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
 
 public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
@@ -24,6 +27,7 @@ public class LoginActivity extends AppCompatActivity {
             }
 
         });
+
 
 
     }
