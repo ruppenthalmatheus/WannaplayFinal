e663d08bd4e85061603ba5720c932cfbf261675f
Matheus Ruppenthal <ruppenthalmatheus@gmail.com>
2018-10-30 01:20:47 -0300

    Account creation with Firestore

--- app/src/main/java/com/example/matheus/wannaplay/RegisterActivity.java
+++ app/src/main/java/com/example/matheus/wannaplay/RegisterActivity.java
@@ -1,11 +1,11 @@
 package com.example.matheus.wannaplay;
 
-
 import android.Manifest;
 import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
+import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
@@ -14,36 +14,43 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.DatePicker;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.matheus.wannaplay.Utilities.Tags;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.Calendar;
+import java.util.HashMap;
 import java.util.Locale;
+import java.util.Map;
 
 public class RegisterActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
 
     private String mMusicianName;
     private String mMusicianPhotoUrl;
     private int mMusicianAge;
-    private double mMusicianLatitude;
-    private double mMusicianLongitude;
-    private boolean mMusicianVocal;
-    private boolean mMusicianGuitar;
-    private boolean mMusicianBass;
-    private boolean mMusicianDrums;
-    private boolean mMusicianOthers;
+    private double mMusicianLatitude, mMusicianLongitude;
+    private boolean mMusicianVocal, mMusicianGuitar, mMusicianBass, mMusicianDrums, mMusicianOthers;
+    private Calendar calendar;
     private TextView displayDate;
+    private String userName;
+    private CheckBox mVocalBtn, mGuitarBtn, mBassBtn, mDrumsBtn, mOthersBtn;
     private DatePickerDialog datePickerDialog;
     private FusedLocationProviderClient fusedLocationProviderClient;
     private GoogleApiClient googleApiClient;
+    private FirebaseFirestore firebaseFirestore = FirebaseFirestore.getInstance();
+    private FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,7 +65,11 @@ public class RegisterActivity extends AppCompatActivity implements GoogleApiClie
 
         fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
 
-        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+        mVocalBtn = findViewById(R.id.registerVocalCheckbox);
+        mGuitarBtn = findViewById(R.id.registerGuitarCheckbox);
+        mBassBtn = findViewById(R.id.registerBassCheckbox);
+        mDrumsBtn = findViewById(R.id.registerDrumCheckbox);
+        mOthersBtn = findViewById(R.id.registerOtherCheckbox);
 
         Button finishButton = findViewById(R.id.registerFinishBtn);
         displayDate = findViewById(R.id.date_select);
@@ -68,7 +79,7 @@ public class RegisterActivity extends AppCompatActivity implements GoogleApiClie
             finishButton.setBackgroundResource(R.drawable.btn_finish_br);
         }
 
-        String userName = firebaseAuth.getCurrentUser().getDisplayName();
+        userName = firebaseAuth.getCurrentUser().getDisplayName();
         String userFirstName = userName.substring(0, userName.indexOf(" "));
 
         TextView welcomeMessage = findViewById(R.id.welcome_message);
@@ -106,6 +117,7 @@ public class RegisterActivity extends AppCompatActivity implements GoogleApiClie
                 if (displayDate.getText().toString().equals("")) {
                     Toast.makeText(RegisterActivity.this, getResources().getString(R.string.registerRequiredBirthMessage), Toast.LENGTH_SHORT).show();
                 } else {
+                    saveMusician();
                     Intent intent = new Intent(getApplicationContext(), MainActivity.class);
                     finish();
                     startActivity(intent);
@@ -113,6 +125,78 @@ public class RegisterActivity extends AppCompatActivity implements GoogleApiClie
             }
         });
 
+    }
+
+    /*private int calculateAge(long date) {
+        Calendar c = Calendar.getInstance();
+        c.setTimeInMillis(date);
+        Calendar today = Calendar.getInstance();
+
+        int age = today.get((Calendar.YEAR) - c.get(Calendar.YEAR));
+
+        if (today.get(Calendar.DAY_OF_MONTH) < c.get(Calendar.DAY_OF_MONTH)) {
+            age--;
+        }
+
+        return age;
+    }*/
+
+    private void saveMusician() {
+
+        final Tags t = new Tags();
+
+        final boolean isVocalist = mVocalBtn.isChecked();
+        final boolean isGuitarist = mGuitarBtn.isChecked();
+        final boolean isBassist = mBassBtn.isChecked();
+        final boolean isDrummer = mDrumsBtn.isChecked();
+        final boolean isOthers = mOthersBtn.isChecked();
+        calendar = Calendar.getInstance();
+        //int age = calculateAge(calendar.getTimeInMillis());
+        String photoUrl = String.valueOf(firebaseAuth.getCurrentUser().getPhotoUrl());
+
+        Map<String, Object> musician = new HashMap<>();
+        musician.put(t.getKEY_NAME(), userName);
+        musician.put(t.getKEY_EMAIL(), firebaseAuth.getCurrentUser().getEmail());
+        musician.put(t.getKEY_PHOTO(), photoUrl);
+        musician.put(t.getKEY_AGE(), 28);
+        musician.put(t.getKEY_LATITUDE(), mMusicianLatitude);
+        musician.put(t.getKEY_LONGITUDE(), mMusicianLongitude);
+
+        firebaseFirestore.collection(t.getKEY_MUSICIANS())
+                .add(musician)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Map<String, Object> skills = new HashMap<>();
+                        skills.put(t.getKEY_VOCAL(), isVocalist);
+                        skills.put(t.getKEY_GUITAR(), isGuitarist);
+                        skills.put(t.getKEY_BASS(), isBassist);
+                        skills.put(t.getKEY_DRUMS(), isDrummer);
+                        skills.put(t.getKEY_OTHERS(), isOthers);
+
+                        documentReference.collection(t.getKEY_SKILLS())
+                                .add(skills)
+                                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                                    @Override
+                                    public void onSuccess(DocumentReference documentReference) {
+                                        Log.d("OK", "Skills successfully added!");
+                                    }
+                                })
+                                .addOnFailureListener(new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        Log.w("ERROR", "Error adding the skills", e);
+                                    }
+                                });
+                        Log.d("OK", "DocumentSnapshot added with ID: " + documentReference.getId());
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w("ERROR", "Error adding document", e);
+                    }
+                });
     }
 
     @Override
---
+++ app/src/main/java/com/example/matheus/wannaplay/Utilities/Tags.java
@@ -0,0 +1,78 @@
+package com.example.matheus.wannaplay.Utilities;
+
+public class Tags {
+
+    /* MUSICIAN TAGS */
+
+    private String KEY_NAME = "name";
+    private String KEY_PHOTO = "photoUrl";
+    private String KEY_EMAIL = "email";
+    private String KEY_AGE = "age";
+    private String KEY_LATITUDE = "latitude";
+    private String KEY_LONGITUDE = "longitude";
+
+    /* SKILLS TAGS */
+
+    private String KEY_VOCAL = "vocal";
+    private String KEY_GUITAR = "guitar";
+    private String KEY_BASS = "bass";
+    private String KEY_DRUMS = "drums";
+    private String KEY_OTHERS = "others";
+
+    /* COLLECTIONS TAGS */
+
+    private String KEY_MUSICIANS = "musicians";
+    private String KEY_SKILLS = "skills";
+
+    public String getKEY_NAME() {
+        return KEY_NAME;
+    }
+
+    public String getKEY_EMAIL() {
+        return KEY_EMAIL;
+    }
+
+    public String getKEY_PHOTO() {
+        return KEY_PHOTO;
+    }
+
+    public String getKEY_AGE() {
+        return KEY_AGE;
+    }
+
+    public String getKEY_LATITUDE() {
+        return KEY_LATITUDE;
+    }
+
+    public String getKEY_LONGITUDE() {
+        return KEY_LONGITUDE;
+    }
+
+    public String getKEY_VOCAL() {
+        return KEY_VOCAL;
+    }
+
+    public String getKEY_GUITAR() {
+        return KEY_GUITAR;
+    }
+
+    public String getKEY_BASS() {
+        return KEY_BASS;
+    }
+
+    public String getKEY_DRUMS() {
+        return KEY_DRUMS;
+    }
+
+    public String getKEY_OTHERS() {
+        return KEY_OTHERS;
+    }
+
+    public String getKEY_MUSICIANS() {
+        return KEY_MUSICIANS;
+    }
+
+    public String getKEY_SKILLS() {
+        return KEY_SKILLS;
+    }
+}
